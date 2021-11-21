d36adbbb2e269b26a81b10243cddbdb2989ee37e
Renchindorj Batzorig <renchindorj.n@nmma.co>
2021-11-21 17:27:17 +0800

    Renchin

--- src/components/knowledgeBase/Kbase.js
+++ src/components/knowledgeBase/Kbase.js
@@ -1,92 +1,106 @@
 import { Row, Col } from "antd";
 import React from "react";
+import getData from "../../utils";
 
-export const Kbase = () => {
+export const Kbase = ({
+  knowledgeBase,
+  knowledge,
+  barimt1,
+  barimt2,
+  barimt3,
+  barimtlast,
+}) => {
+  console.log(knowledgeBase, "==========");
+  console.log(knowledge, "aaaaaaaaaa");
+  console.log(barimt1, "eeeeeeee");
+  console.log(barimtlast, "aewwewewwe");
+
   return (
     <Row className="kbase">
-      <Col span={12}>
-        <div className="page">
-          <div className="div-button">
-            <button className="clicked" type="button">
-              Хууль,эрх зүй
-            </button>
-            <button type="button">Судалгаа</button>
-            <button type="button">Сургалт</button>
-            <button type="button">Гарын авлага</button>
-            <button type="button">Холбоотой цахим хуудсууд</button>
-          </div>
-          <p className="title">
-            <b>Эрх зүйн хүрээ</b>
-          </p>
-          <div className="line"></div>
-          <p className="">
-            Монгол улсын хүчин төгөлдөр хууль, холбогдох захиргааны хэм
-            хэмжээний актад тусгасан төрийн байгууллага, нутгийн өөрөө удирдах
-            байгууллагаас шийдвэр гаргахад Иргэдийн Оролцоог хангах аливаа
-            зохицуулалтын өнөөгийн хэрэгжилтийн байдалд хийсэн дүн шинжилгээ
-          </p>
-          <p>
-            Монгол Улсын Засгийн газар, Щвейцарийн Холбооны Улсын Засгийн газар
-            хоорондын хэлэлцээрийн дагуу хэрэгжиж буй “Засаглалыг дэмжих,
-            төвлөрлийг сааруулах хөтөлбөр”-ийн хоёр дахь үе шатанд хэрэгжүүлж
-            буй “Иргэдийн оролцоо” төслийн нэгжээс Монгол Улсын хүчин төгөлдөр
-            бүх хууль, холбогдох бусад эрх зүйн актанд тусгасан төрийн
-            байгууллага, нутгийн өөрөө удирдах байгууллагаас бүх түвшинд шийдвэр
-            гаргахдаа иргэдийг оролцуулах аливаа зохицуулалтын өнөөгийн
-            хэрэгжилтийн байдалд мониторинг хийх, холбогдох хууль, эрх зүйн
-            актын уялдаа холбоо болон зөрчил, давхардал, хийдлийг тодорхойлох,
-            иргэний оролцооны эрх зүйн орчны нөхцөл байдлыг цогц байдлаар үнэлж,
-            дүгнэн шинжилсэн энэхүү судалгааг хийлгэлээ.
-          </p>
-          <div className="files-container">
-            <div className="files">
-              <h4>Баримт, бичиг</h4>
+      <div className="div-button">
+        <button className="clicked" type="button">
+          Хууль,эрх зүй
+        </button>
+        <button type="button">Судалгаа</button>
+        <button type="button">Сургалт</button>
+        <button type="button">Гарын авлага</button>
+        <button type="button">Холбоотой цахим</button>
+      </div>
+      <Col span={15}>
+        <p className="title">
+          <b>{knowledgeBase.title.rendered}</b>
+        </p>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: knowledgeBase.content.rendered,
+          }}
+        />
+        <Row className="files-container">
+          <Row gutter={8}>
+            <Col span={8} className="files">
+              <h4>{barimt1.title.rendered}</h4>
               <div className="center">
-                <img src="#" alt="PDF" />
+                <img src={getData(barimt1._embedded, "image")} />
                 <a href="#" download>
-                  Судалгааны тайлан
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: barimt1.content.rendered,
+                    }}
+                  />
                 </a>
               </div>
-            </div>
-            <div className="files">
-              <h4>Баримт, бичиг</h4>
+            </Col>
+
+            <Col span={8} className="files">
+              <h4>{barimt2.title.rendered}</h4>
               <div className="center">
-                <img src="#" alt="PDF" />
+                <img src={getData(barimt2._embedded, "image")} />
                 <a href="#" download>
-                  Судалгааны тайлан
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: barimt2.content.rendered,
+                    }}
+                  />
                 </a>
               </div>
-            </div>
-            <div className="files">
-              <h4>Баримт, бичиг</h4>
+            </Col>
+
+            <Col span={8} className="files">
+              <h4>{barimt3.title.rendered}</h4>
               <div className="center">
-                <img src="#" alt="PDF" />
+                <img src={getData(barimt3._embedded, "image")} />
                 <a href="#" download>
-                  Судалгааны тайлан
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: barimt3.content.rendered,
+                    }}
+                  />
                 </a>
               </div>
-            </div>
-          </div>
-          <p className="title">
-            <b>Иргэдийн оролцоо бүхий хуулиудын жагсаалт</b>
+            </Col>
+          </Row>
+          <p className="subtitle">
+            <b>{knowledge.title.rendered}</b>
           </p>
-          <p>
-            2016 оны 2 сарын 4-ний байдлаар Монгол улсад нийт 459 хууль хүчин
-            төгөлдөр үйлчилж байгаагаас 105 хуульд (нийт хуулийн дөрөвний нэг
-            орчим) иргэдийн оролцоог хэсэгчлэн зохицуулсан заалт байна. Иргэдийн
-            оролцоо бүхий эдгээр хуулиудыг доор жагсаав.
-          </p>
-          <div className="files lastFile">
-            <h4>Баримт, бичиг</h4>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: knowledge.content.rendered,
+            }}
+          />
+          <Col span={15} className="lastFile">
+            <h4>{barimtlast.title.rendered}</h4>
             <div className="center">
-              <img src="#" alt="PDF" />
+              <img src={getData(barimtlast._embedded, "image")} />
               <a href="#" download>
-                Иргэдийн оролцоо бүхий хуулиудын жагсаалт
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: barimtlast.content.rendered,
+                  }}
+                />
               </a>
             </div>
-          </div>
-          <div className="line bottom"></div>
-        </div>
+          </Col>
+        </Row>
       </Col>
     </Row>
   );
--- src/pages/knowledgebase.js
+++ src/pages/knowledgebase.js
@@ -1,13 +1,85 @@
 import Layout from "../components/layouts/Layout";
 import React from "react";
+import WPAPI from "wpapi";
+import Config from "../config";
 import { Kbase } from "../components/knowledgeBase/Kbase";
-const Knowledgebase = () => {
+
+const wp = new WPAPI({ endpoint: Config.apiUrl });
+
+const Knowledgebase = ({
+  knowledgeBase,
+  knowledge,
+  barimt1,
+  barimt2,
+  barimt3,
+  barimtlast,
+}) => {
   return (
     <Layout>
       <div className="page knowledgebase">
-        <Kbase/>
+        <Kbase
+          knowledgeBase={knowledgeBase}
+          knowledge={knowledge}
+          barimt1={barimt1}
+          barimt2={barimt2}
+          barimt3={barimt3}
+          barimtlast={barimtlast}
+        />
       </div>
     </Layout>
   );
 };
+
+Knowledgebase.getInitialProps = async () => {
+  const knowledgeBase = await wp
+    .posts()
+    .slug("knowledgeBase")
+    .embed()
+    .then((data) => {
+      return data[0];
+    });
+
+  const knowledge = await wp
+    .posts()
+    .slug("knowledge")
+    .embed()
+    .then((data) => {
+      return data[0];
+    });
+
+  const barimt1 = await wp
+    .posts()
+    .slug("barimt1")
+    .embed()
+    .then((data) => {
+      return data[0];
+    });
+
+  const barimt2 = await wp
+    .posts()
+    .slug("barimt2")
+    .embed()
+    .then((data) => {
+      return data[0];
+    });
+
+  const barimt3 = await wp
+    .posts()
+    .slug("barimt3")
+    .embed()
+    .then((data) => {
+      return data[0];
+    });
+
+  const barimtlast = await wp
+    .posts()
+    .slug("barimtlast")
+    .embed()
+    .then((data) => {
+      return data[0];
+    });
+
+  return { knowledgeBase, knowledge, barimt1, barimt2, barimt3, barimtlast };
+};
+
 export default Knowledgebase;
