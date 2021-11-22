05d0d44f456e84057a01d6d734df7b224002c5a0
Renchindorj Batzorig <renchindorj.n@nmma.co>
2021-11-22 11:58:11 +0800

    partners fixed

--- src/components/partners/Partners.js
+++ src/components/partners/Partners.js
@@ -3,207 +3,67 @@ import React from "react";
 import getData from "../../utils";
 
 export const Partner = ({
-  partners,
-  bigBox1,
-  bigBox2,
-  bigBox3,
-  bigBox4,
-  smallbox1,
-  smallbox2,
-  smallbox3,
-  smallbox4,
-  smallbox5,
-  smallbox6,
+  partner_organizationsCat,
+  partner_organizationsPosts,
+  partners_orgCat,
+  partners_orgPosts,
 }) => {
   return (
     <Row className="partner">
       <Col span={24}>
-        <h2>{partners.title.rendered}</h2>
-        <div className="boxContainer">
-          <div className="bigBox">
-            <img
-              src={getData(bigBox1._embedded, "image")}
-              className="headPic"
-            />
-            <div
-              dangerouslySetInnerHTML={{
-                __html: bigBox1.content.rendered,
-              }}
-            />
-             <div className="nextButton">
-              <p>visit website</p>
-              <a href="#">
-                <img src="/images/home/Button.png" />
-              </a>
-            </div>
-          </div>
-          <div className="bigBox">
-            <img
-              src={getData(bigBox2._embedded, "image")}
-              className="headPic"
-            />
-            <div
-              dangerouslySetInnerHTML={{
-                __html: bigBox2.content.rendered,
-              }}
-            />
-             <div className="nextButton">
-              <p>visit website</p>
-              <a href="#">
-                <img src="/images/home/Button.png" />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="boxContainer">
-          <div className="bigBox">
-            <img
-              src={getData(bigBox3._embedded, "image")}
-              className="headPic"
-            />
-            <div
-              dangerouslySetInnerHTML={{
-                __html: bigBox3.content.rendered,
-              }}
-            />
-             <div className="nextButton">
-              <p>visit website</p>
-              <a href="#">
-                <img src="/images/home/Button.png" />
-              </a>
-            </div>
-          </div>
-          <div className="bigBox">
-            <img
-              src={getData(bigBox4._embedded, "image")}
-              className="headPic"
-            />
-            <div
-              dangerouslySetInnerHTML={{
-                __html: bigBox4.content.rendered,
-              }}
-            />
-             <div className="nextButton">
-              <p>visit website</p>
-              <a href="#">
-                <img src="/images/home/Button.png" />
-              </a>
-            </div>
-          </div>
-        </div>
-        <h2>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: partners.content.rendered,
-            }}
-          />
-        </h2>
-        <div className="boxContainer">
-          <div className="smallBox">
-            <img
-              src={getData(smallbox1._embedded, "image")}
-              className="headPic"
-            />
-            <div
-              dangerouslySetInnerHTML={{
-                __html: smallbox1.content.rendered,
-              }}
-            />
-             <div className="nextButton">
-              <p>visit website</p>
-              <a href="#">
-                <img src="/images/home/Button.png" />
-              </a>
-            </div>
-          </div>
-          <div className="smallBox">
-            <img
-              src={getData(smallbox2._embedded, "image")}
-              className="headPic"
-            />
-            <div
-              dangerouslySetInnerHTML={{
-                __html: smallbox2.content.rendered,
-              }}
-            />
-             <div className="nextButton">
-              <p>visit website</p>
-              <a href="#">
-                <img src="/images/home/Button.png" />
-              </a>
-            </div>
-          </div>
-          <div className="smallBox">
-            <img
-              src={getData(smallbox3._embedded, "image")}
-              className="headPic"
-            />
-            <div
-              dangerouslySetInnerHTML={{
-                __html: smallbox3.content.rendered,
-              }}
-            />
-             <div className="nextButton">
-              <p>visit website</p>
-              <a href="#">
-                <img src="/images/home/Button.png" />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="boxContainer">
-          <div className="smallBox">
-            <img
-              src={getData(smallbox4._embedded, "image")}
-              className="headPic"
-            />
-            <div
-              dangerouslySetInnerHTML={{
-                __html: smallbox4.content.rendered,
-              }}
-            />
-             <div className="nextButton">
-              <p>visit website</p>
-              <a href="#">
-                <img src="/images/home/Button.png" />
-              </a>
-            </div>
-          </div>
-          <div className="smallBox">
-            <img
-              src={getData(smallbox5._embedded, "image")}
-              className="headPic"
-            />
-            <div
-              dangerouslySetInnerHTML={{
-                __html: smallbox5.content.rendered,
-              }}
-            />
-             <div className="nextButton">
-              <p>visit website</p>
-              <a href="#">
-                <img src="/images/home/Button.png" />
-              </a>
-            </div>
-          </div>
-          <div className="smallBox lastBox">
-            <img
-              src={getData(smallbox6._embedded, "image")}
-              className="headPic"
-            />
-            <div
-              dangerouslySetInnerHTML={{
-                __html: smallbox6.content.rendered,
-              }}
-            />
-            <div className="nextButton">
-              <p>visit website</p>
-              <a href="#">
-                <img src="/images/home/Button.png" />
-              </a>
-            </div>
-          </div>
-        </div>
+        <h2>{partner_organizationsCat.name}</h2>
+        <Row className="boxContainer">
+          {partner_organizationsPosts.map((word, ind) => {
+            return (
+              <Col span={12} key={ind} className="text">
+                <div className="bigBox">
+                  <img
+                    src={getData(word._embedded, "image")}
+                    className="headPic"
+                  />
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: word.content.rendered,
+                    }}
+                  />
+                  <div className="nextButton">
+                    <p>visit website</p>
+                    <a href="#">
+                      <img src="/images/home/Button.png" />
+                    </a>
+                  </div>
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+
+        <h2>{partners_orgCat.name}</h2>
+        <Row className="boxContainer">
+          {partners_orgPosts.map((word, ind) => {
+            return (
+              <Col span={8} key={ind} className="text">
+                <div className="smallBox">
+                  <img
+                    src={getData(word._embedded, "image")}
+                    className="headPic"
+                  />
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: word.content.rendered,
+                    }}
+                  />
+                  <div className="nextButton">
+                    <p>visit website</p>
+                    <a href="#">
+                      <img src="/images/home/Button.png" />
+                    </a>
+                  </div>
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
       </Col>
     </Row>
   );
